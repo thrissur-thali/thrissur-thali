3a22bbf52e23b1129835cfb51e4fd514bc5acacc
thrissur-thali <thrissurthali@gmail.com>
2025-03-29 00:04:24 +0530

    Update script.js
    restored my console log

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ async function fetchData() {
 
         parsedData.forEach(row => {
             const tr = document.createElement('tr');
-            console.log("Arpitha");
             Object.values(row).forEach(cell => {
                 const td = document.createElement('td');
                 td.textContent = cell;
